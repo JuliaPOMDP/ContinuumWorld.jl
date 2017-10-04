47f84f7038621425ce5b82e860ba2ffb97d19814
Zachary Sunberg <sunbergzach@gmail.com>
2017-10-04 09:54:14 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # ContinuumWorld
 
 [![Build Status](https://travis-ci.org/zsunberg/ContinuumWorld.jl.svg?branch=master)](https://travis-ci.org/zsunberg/ContinuumWorld.jl)
-
 [![Coverage Status](https://coveralls.io/repos/zsunberg/ContinuumWorld.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/zsunberg/ContinuumWorld.jl?branch=master)
-
 [![codecov.io](http://codecov.io/github/zsunberg/ContinuumWorld.jl/coverage.svg?branch=master)](http://codecov.io/github/zsunberg/ContinuumWorld.jl?branch=master)
+
+A simple continuous gridworld-like MDP and solver for education purposes. See [the Demo notebook](https://nbviewer.jupyter.org/github/zsunberg/ContinuumWorld.jl/blob/master/notebooks/Demo.ipynb) or [POMDPGallery](https://github.com/JuliaPOMDP/POMDPGallery.jl) for usage examples.
+
+![Function approximation value iteration](img/out.gif)
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using ContinuumWorld
 using POMDPs
 using POMDPToolbox
 using Base.Test
+using Plots
 
 w = CWorld()
 
@@ -13,9 +14,12 @@ pol = solve(sol, w)
 sim = HistoryRecorder(rng=MersenneTwister(5), max_steps=30)
 @show state_hist(simulate(sim, w, pol))
 
+#=
 write_file(CWorldVis(w, f=norm), joinpath(tempdir(), "test.png"))
 @time write_file(CWorldVis(w, f=norm, g=sol.grid), joinpath(tempdir(), "test_timed.png"))
 @time write_file(CWorldVis(w, f=norm), joinpath(tempdir(), "test_timed.tif"))
 @time write_file(CWorldVis(w, f=norm, g=sol.grid), joinpath(tempdir(), "test_timed.tif"))
 @time write_file(CWorldVis(w, f=s->action_ind(pol, s), g=sol.grid, title="Policy"), joinpath(tempdir(), "policy.png"))
 @time write_file(CWorldVis(w, f=s->value(pol, s), g=sol.grid, title="Value"), joinpath(tempdir(), "value.png"))
+=#
+plot(CWorldVis(w, f=s->value(pol, s), g=sol.grid, title="Value"))
